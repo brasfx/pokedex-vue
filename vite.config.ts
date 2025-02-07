410bb27580f9d78b99a1d5c783fbb818f95c355e
Davi Ribeiro <foxpc18@gmail.com>
2025-02-07 14:49:12 -0300

    Install and configure sass into project

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+	plugins: [vue(), vueJsx(), vueDevTools()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "scss/app.scss";`,
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 })
